68c2c88d13fae2accdf2ff9f2a62d0150c681c30
henry dewey <hdewey02@gmail.com>
2023-09-17 16:50:42 -0700

    fixed closing bugs

--- src/components/Log.tsx
+++ src/components/Log.tsx
@@ -4,7 +4,8 @@ import theme from '../styles/theme.module.css';
 
 import styles from "../styles/Log.module.css";
 import Loader from "./Loader";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { timeSince } from "../utils/etc";
 
 const Log = () => {
 
@@ -12,13 +13,21 @@ const Log = () => {
 
   const [ openedCard, setOpenedCard ] = useState<null | number>(null);
 
+  const handleCardClick = (index: number) => {
+    const selection = window.getSelection();
+    if (!selection || selection.toString().length === 0) {
+      setOpenedCard(prevIndex => prevIndex === index ? null : index);
+    }
+  };
+
+
   return (
     <>
       <div style={{marginTop: '5vh', width: '100%'}}>
         { 
           properties ? properties.map( (property: any, index: number) => {
             return (
-              <div key={index} className={styles.cardWrapper} onClick={() => setOpenedCard(index)}>
+              <div key={index} className={styles.cardWrapper} onClick={() => handleCardClick(index)}>
                 <PropertyInfoCard isOpen={openedCard === index ? true : false} property={property} />
               </div>
             )
@@ -41,51 +50,85 @@ const PropertyInfoCard = ({ isOpen, property }: { isOpen: boolean, property: any
   const [ isDescriptionsOpen, setIsDescriptionsOpen ] = useState(false);
   const toggleIsDescriptionsOpen = () => setIsDescriptionsOpen(!isDescriptionsOpen);
 
+  const [lastUpdated, setLastUpdated] = useState<null | Date>(null);
+
+  const handleMouseDown = (e: any) => {
+    e.stopPropagation();
+  };
+
+  useEffect(() => {
+    if (propertyInfo) {
+      const max_unix_ts = Math.max(
+        ...propertyInfo.transcripts.map((t: any) => t.upload_time),
+        ...propertyInfo.descriptions.map((d: any) => d.upload_time)
+      )
+
+      if (propertyInfo.transcripts.length + propertyInfo.descriptions.length > 0 ) {
+        const latestUploadDate = new Date(max_unix_ts * 1000);
+        setLastUpdated(latestUploadDate);
+      }
+    }
+  }, [propertyInfo])
+
   function convertUnixTimestampToDate(unixTimestamp: number) {
     const date = new Date(unixTimestamp * 1000); 
     return `${date.toDateString()} ${date.toTimeString().split(' ')[0]}`; 
   }
 
   return (
-    <div className={styles.propertyBox}>
-        <div className={styles.statusRow}>
-          <h2 className={styles.propertyName}>{property.property_name}</h2>
-          <h2>{property.agent}</h2>
+    <div className={styles.propertyBox} onMouseDown={handleMouseDown}>
+      <div className={styles.closedCardRow}>
+        <div>
+          <h1>{property.property_name}</h1>
+          <p>Agent: {property.agent}</p>
         </div>
+        <div>
+          {/* {convertUnixTimestampToDate(property.uploaded_at)} */}
+          { lastUpdated && <div>Last modified: {timeSince(lastUpdated)}</div> }
+          { propertyInfo && !lastUpdated && <div>{"No modifications"}</div> }
+        </div>
+      </div>
+        {/* {
+          !isOpen ? 
+            <>
+            </>
+          : (
+            <>
+            </>
+          )
+        } */}
         {
           propertyInfo && isOpen ?
             <>
-              { propertyInfo.transcripts && 
+              {propertyInfo.transcripts && 
                 <div>
-                  <h2>Transcriptions: ({propertyInfo.transcripts.length})</h2>
+                  <h2 className={styles.sectionTitle}>Transcriptions: ({propertyInfo.transcripts.length})</h2>
                   <div className={styles.listContainer}>
                     {propertyInfo.transcripts.map((transcript: any, index: number) => (
-                      <>
-                        <h1 className={styles.taskNumber}>{index + 1}</h1>
-
-                        <div key={index} className={styles.dataBox}>
+                      <div key={index} className={styles.dataBox}>
+                        <h1 className={styles.index}>{index + 1}.</h1>
+                        <div className={styles.content}>
                           <h3 className={styles.timestampHeader}>{convertUnixTimestampToDate(transcript.upload_time)}</h3>
                           <div className={styles.textResult}>{transcript.transcription}</div>
                         </div>
-                      </>
+                      </div>
                     ))}
                   </div>
                 </div>
               }
 
-              { propertyInfo.descriptions && 
+              {propertyInfo.descriptions && 
                 <div>
-                  <h2>Descriptions: ({propertyInfo.descriptions.length})</h2>
+                  <h2 className={styles.sectionTitle}>Descriptions: ({propertyInfo.descriptions.length})</h2>
                   <div className={styles.listContainer}>
                     {propertyInfo.descriptions.map((desc: any, index: number) => (
-                      <>
-                        <h1 className={styles.taskNumber}>{index + 1}</h1>
-
-                        <div key={index} className={styles.dataBox}>
+                      <div key={index} className={styles.dataBox}>
+                        <h1 className={styles.index}>{index + 1}.</h1>
+                        <div className={styles.content}>
                           <h3 className={styles.timestampHeader}>{convertUnixTimestampToDate(desc.upload_time)}</h3>
                           <div className={styles.textResult}>{desc.descriptions}</div>
                         </div>
-                      </>
+                      </div>
                     ))}
                   </div>
                 </div>
---
+++ src/utils/etc.ts
@@ -0,0 +1,21 @@
+export function timeSince(date: Date) {
+  const seconds = Math.floor((new Date().getTime() - date.getTime()) / 1000);
+  
+  const intervals = [
+    { name: 'year', duration: 31536000 },
+    { name: 'month', duration: 2592000 },
+    { name: 'day', duration: 86400 },
+    { name: 'hour', duration: 3600 },
+    { name: 'minute', duration: 60 }
+  ];
+
+  for (let i = 0; i < intervals.length; i++) {
+    const currentInterval = seconds / intervals[i].duration;
+    if (currentInterval >= 1) {
+      const value = Math.floor(currentInterval);
+      return `${value} ${intervals[i].name}${value !== 1 ? 's' : ''} ago`;
+    }
+  }
+
+  return Math.floor(seconds) + " seconds ago";
+}
